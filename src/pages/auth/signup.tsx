87f7bf0665fa25c2c2220dccb6613ca4f9663448
nattyengeda <nattynengeda@gmail.com>
2024-01-10 19:26:49 +0300

    Update Today

--- src/pages/auth/signup.tsx
+++ src/pages/auth/signup.tsx
@@ -1,6 +1,9 @@
 import {
   LockOutlined
 } from "@mui/icons-material"
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
 
 import {
   Box,
@@ -13,7 +16,23 @@ import {
 
 import { Link } from "react-router-dom";
 
+const schema = yup.object({
+  email: yup.string().required().email(),
+  fullname: yup.string().required(),
+  password: yup.string().required(),
+}).required();
+
 export default function Signup() {
+
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(schema)
+  });
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  }
+
+
   return (
     <main className='w-full min-h-screen h-full flex flex-col gap-20 items-center justify-between py-20'>
 
@@ -29,6 +48,7 @@ export default function Signup() {
         </div>
         <Box
           component={'form'}
+          onSubmit={handleSubmit(onSubmit)}
           sx={{
             // '& > :not(style)': { m: 1, width: '50ch' },
             width: '300px',
@@ -54,43 +74,59 @@ export default function Signup() {
           autoComplete="off">
 
           {/* Full Name */}
-          <TextField
-            sx={{
-              width: '100%'
-            }}
-            id="text"
-            label="Full Name"
-            variant="outlined"
-            required
-            autoComplete="off"
-          />
+          <div className="w-full flex flex-col gap-2 items-start">
+            <TextField
+              sx={{
+                width: '100%'
+              }}
+              id="text"
+              label="Full Name"
+              variant="outlined"
+              required
+              autoComplete="off"
+              {...register('fullname', { required: true, maxLength: 80, minLength: 2, pattern: /^[A-Za-z]+$/i })}
+            />
+            {errors.fullname && <span className="text-sm text-red-500">This field is required</span>}
+            {errors.fullname && errors.fullname.type === "maxLength" && <span className="text-sm text-red-500">Max length exceeded</span>}
+            {errors.fullname && errors.fullname.type === "minLength" && <span className="text-sm text-red-500">Min length exceeded</span>}
+            {errors.fullname && errors.fullname.type === "pattern" && <span className="text-sm text-red-500">Alphabetical characters only</span>}
+
+          </div>
 
           {/* Email */}
-          <TextField
-            sx={{
-              width: '100%'
-            }}
-            id="email"
-            label="Email Address"
-            variant="outlined"
-            required
-            autoComplete="off"
-          />
+          <div className="w-full flex flex-col gap-2 items-start">
+            <TextField
+              sx={{
+                width: '100%'
+              }}
+              id="email"
+              label="Email Address"
+              variant="outlined"
+              required
+              autoComplete="off"
+              {...register('email', { required: true, pattern: /^\S+@\S+$/i })}
+            />
+            {errors.email && <span className="text-sm text-red-500">This field is required</span>}
+            {errors.email && errors.email.type === "pattern" && <span className="text-sm text-red-500">Invalid email format</span>}
+          </div>
 
           {/* Password */}
-          <TextField
-            sx={{
-              width: '100%'
-            }}
-            id="password"
-            label="Password"
-            type="password"
-          />
-
-
+          <div className="w-full flex flex-col gap-2 items-start">
+            <TextField
+              sx={{
+                width: '100%'
+              }}
+              id="password"
+              label="Password"
+              type="password"
+              {...register('password')}
+            />
+            {errors.password && <span className="text-sm text-red-500">This field is required</span>}
+          </div>
 
           {/* Sign in Button */}
           <Button
+            type="submit"
             sx={{
               width: '100%',
               height: '50px',
